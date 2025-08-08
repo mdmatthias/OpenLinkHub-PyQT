e41f978d5148ecf1eba23444f73a3d899dcb0118
matthymd <matthias_dillen@telenet.be>
2025-08-08 15:37:16 +0200

    Add openlinkhub qt webview and tray icon

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # OpenLinkHub-PyQT
-OpenLinkHub QT webview app with tray icon
+To be used together with OpenLinkHub https://github.com/jurkovic-nikola/OpenLinkHub
+
+This app loads OpenLinkHub in a QT app instead of a browser tab.
+Also comes with a simple tray icon to open and close the app.
+
+![preview.png](preview.png)
+# Requirements
+```
+Python >= 3.9
+PyQT6 >= 6.9.1
+PyQt6-WebEngine >= 6.9.0
+```
+
+# Environment variables
+If you use the default url for OpenLinkHub you don't need to set anything. 
+Otherwise, set this env var to your OpenLinkHub url:
+
+```
+OLH_URL=http://localhost:27003
+```
+
+# Running the app
+
+```commandline
+python openlinkhub_qt.py &
+```
---
+++ openlinkhub_qt.py
@@ -0,0 +1,71 @@
+import sys
+from os import getenv
+
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QSystemTrayIcon, QMenu
+)
+from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import QUrl
+
+
+class OpenLinkHubWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("OpenLinkHub")
+        self.setGeometry(0, 0, 1024, 768)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl(getenv("OLH_URL", "http://localhost:27003")))
+        self.setCentralWidget(self.browser)
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+
+class OpenLinkHubTray:
+    def __init__(self, app, window):
+        self.app = app
+        self.window = window
+        self.tray = QSystemTrayIcon()
+        self.tray.setIcon(QIcon("icon.png"))
+
+        self.menu = QMenu()
+
+        self.show_action = QAction("Show")
+        self.quit_action = QAction("Quit")
+
+        self.menu.addAction(self.show_action)
+        self.menu.addAction(self.quit_action)
+
+        self.tray.setContextMenu(self.menu)
+        self.tray.setToolTip("OpenLinkHub")
+
+        self.show_action.triggered.connect(self.window.show)
+        self.quit_action.triggered.connect(self.quit_app)
+
+        self.tray.activated.connect(self.icon_clicked)
+
+        self.tray.show()
+
+    def icon_clicked(self, reason):
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:
+            if self.window.isVisible():
+                self.window.hide()
+            else:
+                self.window.show()
+
+    def quit_app(self):
+        self.tray.hide()
+        self.app.quit()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = OpenLinkHubWindow()
+    tray_app = OpenLinkHubTray(app, window)
+
+    window.show()
+    sys.exit(app.exec())
